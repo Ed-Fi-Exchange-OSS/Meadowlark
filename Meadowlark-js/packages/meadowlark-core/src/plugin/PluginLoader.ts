016c31dfc677fdf368e264e859e317ade44207a9
Brad Banister <brad@banisterconsult.com>
2022-04-20 15:09:49 -0500

    mongodb runs, dynamodb doesn't

--- Meadowlark-js/packages/meadowlark-core/src/plugin/PluginLoader.ts
+++ Meadowlark-js/packages/meadowlark-core/src/plugin/PluginLoader.ts
@@ -4,14 +4,32 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 /* eslint-disable global-require */
 import dotenv from 'dotenv';
+import { Logger } from '../helpers/Logger';
 import { MeadowlarkBackendPlugin } from './backend/MeadowlarkBackendPlugin';
 import { NoMeadowlarkBackendPlugin } from './backend/NoMeadowlarkBackendPlugin';
 
 dotenv.config();
 
-const loadedBackendPlugin = process.env.BACKEND_PLUGIN_MODULE
-  ? require(`../packages/${process.env.BACKEND_PLUGIN_MODULE}/index`).initializeBackendPlugin()
-  : NoMeadowlarkBackendPlugin;
+// Default to no backend plugin
+let loadedBackendPlugin: MeadowlarkBackendPlugin = NoMeadowlarkBackendPlugin;
+
+// Dynamic import
+async function loadBackendPlugin(pluginNpmName: string) {
+  return ((await import(pluginNpmName)) as any).initializeBackendPlugin();
+}
+
+// Workaround no top-level await with IIFE
+(async () => {
+  if (process.env.BACKEND_PLUGIN_NAME) {
+    try {
+      loadedBackendPlugin = await loadBackendPlugin(process.env.BACKEND_PLUGIN_NAME);
+    } catch (e) {
+      const message = e instanceof Error ? e.message : 'unknown';
+      Logger.error(`Unable to load backend plugin "${process.env.BACKEND_PLUGIN_NAME}". Error was ${message}`, null);
+      throw e;
+    }
+  }
+})();
 
 export function backendPlugin(): MeadowlarkBackendPlugin {
   return loadedBackendPlugin;
