3d6b0b17146cc5a6f30d8d9a8b173ca7e9aabde5
Steven Arnold <steven.arnold@ed-fi.org>
2022-06-10 14:41:24 -0500

    Updated to correct Postgres Port

--- Meadowlark-js/backends/meadowlark-postgresql-backend/src/repository/Db.ts
+++ Meadowlark-js/backends/meadowlark-postgresql-backend/src/repository/Db.ts
@@ -11,7 +11,7 @@ let dbPool: Pool | null = null;
 
 const dbConfiguration = {
   host: process.env.POSTGRES_HOST ?? 'localhost',
-  port: Number(process.env.POSTGRES_PORT ?? 5555),
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
   user: process.env.POSTGRES_USER ?? 'postgres',
   password: process.env.POSTGRES_PASSWORD ?? 'abcdefgh1!',
   database: 'meadowlark',
