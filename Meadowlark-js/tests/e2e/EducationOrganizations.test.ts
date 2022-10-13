3ffc238ea8fda929c3bf5360f3390141433772d1
AndonyNS <andony91@gmail.com>
2022-10-13 16:58:42 -0600

    Adding abstract entity test

---
+++ Meadowlark-js/tests/e2e/EducationOrganizations.test.ts
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+import { baseURLRequest, Clients, getAccessToken } from './SharedFunctions';
+
+describe('Education Organizations', () => {
+  it("shouldn't allow to post to abstract entity", async () => {
+    await baseURLRequest
+      .post('/v3.3b/ed-fi/educationOrganizations')
+      .auth(await getAccessToken(Clients.Vendor1), { type: 'bearer' })
+      .send({
+        schoolId: 123,
+        categories: [],
+        gradeLevels: [],
+      })
+      .expect(404);
+  });
+});
