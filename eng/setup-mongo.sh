c371fff07380e1c149798e69051d47e821dad404
AndonyNS <andony91@gmail.com>
2022-10-25 08:23:28 -0600

    Adding script to verify that mongo is running

---
+++ eng/setup-mongo.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+end=$((SECONDS+ 5 * 60))
+
+until [[ `docker inspect -f {{.State.Running}} mongo1` == true || $SECONDS -gt $end ]]; do
+    sleep 2;
+done;
+
+if [ `docker inspect -f {{.State.Running}} mongo1` == true ]
+then
+    echo "--- Container is healthy ---"
+else
+    docker ps
+    docker logs mongo1 --tail 50
+    echo "--- Operation timed out. Review container status ---"
+    exit 1
+fi
