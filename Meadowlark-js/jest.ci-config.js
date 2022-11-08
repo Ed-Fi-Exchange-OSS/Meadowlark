7986e3c9bf9576bad2a734b055917859aaa12ea0
AndonyNS <andony91@gmail.com>
2022-11-08 14:48:49 -0600

    [RND-433] Using a single config file for Jest

--- Meadowlark-js/jest.ci-config.js
+++ Meadowlark-js/jest.ci-config.js
@@ -1,23 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { defaults: tsjPreset } = require('ts-jest/presets');
+const defaultConfig = require('./jest.config.js');
 
 module.exports = {
-  transform: tsjPreset.transform,
-  testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
-  transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*meadowlark-.*).*$'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  collectCoverageFrom: ['packages/**/src/**/*.ts', 'backends/**/src/**/*.ts', 'services/**/src/**/*.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 52,
-      functions: 58,
-      lines: 60,
-      statements: 60,
-    },
-  },
-  modulePathIgnorePatterns: ['dist*', 'docs*'],
-  watchPathIgnorePatterns: ['globalConfig'], // jest-mongodb setup
+  ...defaultConfig,
   reporters: [
     'default',
     [
@@ -31,5 +16,4 @@ module.exports = {
       },
     ],
   ],
-  setupFiles: ["dotenv/config"],
 };
