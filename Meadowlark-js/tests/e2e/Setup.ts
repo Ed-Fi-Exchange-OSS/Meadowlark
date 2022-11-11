454ef0ed934ad61e0d5211ca866f9f1dd62b3dd6
AndonyNS <andony91@gmail.com>
2022-11-11 08:26:51 -0600

    [RND-439] Create admin client and getting admin token on test setup

--- Meadowlark-js/tests/e2e/Setup.ts
+++ Meadowlark-js/tests/e2e/Setup.ts
@@ -3,14 +3,50 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-import request from 'supertest';
-import path from 'path';
-import dotenv from 'dotenv';
-import Chance from 'chance';
+const request = require('supertest');
+const path = require('path');
+const dotenv = require('dotenv');
+const Chance = require('chance');
 
-// Setup
-export const chance = new Chance() as Chance.Chance;
+const credentialManager = require('./helpers/Credentials');
+
 dotenv.config({ path: path.join(__dirname, './.env') });
+
+export const chance = new Chance();
 
 export const baseURLRequest = request(process.env.BASE_URL);
 export const rootURLRequest = request(process.env.ROOT_URL);
+
+async function createAdminClient() {
+  return baseURLRequest
+    .post(`/oauth/client`)
+    .send({
+      clientName: 'Admin Client',
+      roles: ['admin'],
+    })
+    .then((response) => {
+      if (response.status !== 201) {
+        return Promise.reject(new Error('Client already exists. Contact administrator for key and secret'));
+      }
+
+      return {
+        key: response.body.client_id,
+        secret: response.body.client_secret,
+      };
+    });
+}
+
+async function getToken(): Promise<void> {
+  if (!process.env.ADMIN_KEY && !process.env.ADMIN_SECRET) {
+    const credentials = await createAdminClient();
+    await credentialManager.setCredentials(credentials);
+  }
+
+  try {
+    await credentialManager.getAccessToken();
+  } catch (error) {
+    throw new Error(`Unable to generate token for admin user. ${error}`);
+  }
+}
+
+module.exports = async () => getToken();
