c5259349d53b22b4f941690d1d56806cabeb4f3c
AndonyNS <andony91@gmail.com>
2022-11-15 17:07:33 -0600

    Removing additional config option no longer needed

--- Meadowlark-js/jest.e2e-config.js
+++ Meadowlark-js/jest.e2e-config.js
@@ -4,6 +4,5 @@ const defaultConfig = require('./jest.config.js');
 module.exports = {
   displayName: "e2e",
   ...defaultConfig,
-	setupFiles: ['dotenv/config'],
   globalSetup: './tests/e2e/Setup.ts'
 }
