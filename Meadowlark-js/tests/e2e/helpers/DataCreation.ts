5725b60ddb2f92d762f72caa852b23d0d2d99904
Stephen A. Fuqua <stephen@safnet.com>
2023-01-17 15:22:22 -0600

    Several new helper methods

--- Meadowlark-js/tests/e2e/helpers/DataCreation.ts
+++ Meadowlark-js/tests/e2e/helpers/DataCreation.ts
@@ -83,3 +83,150 @@ export async function createStudent(studentUniqueId: string) {
     },
   });
 }
+
+export async function createCourse(courseCode: string, educationOrganizationId: number) {
+  await createResource(
+    {
+      endpoint: 'CourseIdentificationSystemDescriptors',
+      role: 'host',
+      body: {
+        codeValue: 'LEA course code',
+        shortDescription: 'LEA course code',
+        description: 'LEA course code',
+        namespace: 'uri://ed-fi.org/CourseIdentificationSystemDescriptor',
+      },
+    },
+    true,
+  );
+
+  return createResource({
+    endpoint: 'courses',
+    role: 'host',
+    body: {
+      courseCode,
+      educationOrganizationReference: {
+        educationOrganizationId,
+      },
+      courseTitle: 'courseTitle',
+      numberOfParts: 1,
+      identificationCodes: [
+        {
+          courseIdentificationSystemDescriptor: 'uri://ed-fi.org/CourseIdentificationSystemDescriptor#LEA course code',
+          courseCatalogURL: 'http://www.GBISD.edu/coursecatalog',
+          identificationCode: 'ALG-1',
+        },
+      ],
+    },
+  });
+}
+
+export async function createSession(schoolId: number, schoolYear: number, sessionName: string) {
+  await createResource(
+    {
+      endpoint: 'TermDescriptors',
+      role: 'host',
+      body: {
+        codeValue: 'Spring',
+        shortDescription: 'Spring',
+        description: 'Spring',
+        namespace: 'uri://ed-fi.org/TermDescriptor',
+      },
+    },
+    true,
+  );
+
+  return createResource({
+    endpoint: 'sessions',
+    role: 'host',
+    body: {
+      schoolReference: {
+        schoolId,
+      },
+      schoolYearTypeReference: {
+        schoolYear,
+      },
+      sessionName,
+      beginDate: '1900-01-01',
+      endDate: '1900-05-01',
+      termDescriptor: 'uri://ed-fi.org/TermDescriptor#Spring',
+      totalInstructionalDays: 200,
+    },
+  });
+}
+
+export async function createCourseOffering(
+  localCourseCode: string,
+  courseCode: string,
+  schoolId: number,
+  schoolYear: number,
+  sessionName: string,
+) {
+  return createResource({
+    endpoint: 'courseOfferings',
+    role: 'host',
+    body: {
+      localCourseCode,
+      courseReference: {
+        courseCode,
+        educationOrganizationId: schoolId,
+      },
+      schoolReference: {
+        schoolId,
+      },
+      sessionReference: {
+        schoolId,
+        schoolYear,
+        sessionName,
+      },
+    },
+  });
+}
+
+export async function createSection(
+  localCourseCode: string,
+  schoolId: number,
+  schoolYear: number,
+  sessionName: string,
+  sectionIdentifier: string,
+) {
+  return createResource({
+    endpoint: 'sections',
+    role: 'host',
+    body: {
+      sectionIdentifier,
+      courseOfferingReference: {
+        localCourseCode,
+        schoolId,
+        schoolYear,
+        sessionName,
+      },
+    },
+  });
+}
+
+export async function createStudentSectionAssociation(
+  localCourseCode: string,
+  schoolId: number,
+  schoolYear: number,
+  sessionName: string,
+  sectionIdentifier: string,
+  studentUniqueId: string,
+) {
+  return createResource({
+    endpoint: 'studentSectionAssociations',
+    role: 'host',
+    body: {
+      beginDate: '2023-01-16',
+      sectionReference: {
+        localCourseCode,
+        schoolId,
+        schoolYear,
+        sessionName,
+        sectionIdentifier,
+      },
+      studentReference: {
+        studentUniqueId,
+      },
+    },
+  });
+}
