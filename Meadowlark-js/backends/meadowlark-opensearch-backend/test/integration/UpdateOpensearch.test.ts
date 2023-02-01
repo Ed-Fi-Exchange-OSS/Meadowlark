d063fc97c3d65f038985c8923ef9acfa01bb7052
AndonyNS <andony91@gmail.com>
2023-01-31 21:21:01 -0600

    [RND-384] Adding all tests to update open search

--- Meadowlark-js/backends/meadowlark-opensearch-backend/test/integration/UpdateOpensearch.test.ts
+++ Meadowlark-js/backends/meadowlark-opensearch-backend/test/integration/UpdateOpensearch.test.ts
@@ -4,18 +4,25 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 import {
+  DeleteRequest,
+  DeleteResult,
   newSecurity,
   NoDocumentInfo,
   PaginationParameters,
   QueryRequest,
   ResourceInfo,
+  UpdateResult,
   UpsertRequest,
   UpsertResult,
 } from '@edfi/meadowlark-core';
 import { Client } from '@opensearch-project/opensearch/.';
 import { getNewClient } from '../../src/repository/Db';
 import { queryDocuments } from '../../src/repository/QueryOpensearch';
-import { afterUpsertDocument } from '../../src/repository/UpdateOpensearch';
+import {
+  afterDeleteDocumentById,
+  afterUpdateDocumentById,
+  afterUpsertDocument,
+} from '../../src/repository/UpdateOpensearch';
 import { setupOpenSearch, teardownOpenSearch } from '../setup/OpenSearchSetup';
 
 jest.setTimeout(120_000);
@@ -49,40 +56,133 @@ const setupQueryRequest = (queryParameters: any, paginationParameters: Paginatio
 
 describe('given the upsert of a new document', () => {
   let client: Client;
-  let upsertResult: UpsertResult;
 
   beforeAll(async () => {
     await setupOpenSearch();
-
     client = await getNewClient();
   });
 
-  describe('when upsert was successful', () => {
-    beforeAll(async () => {
-      upsertResult = {
-        response: 'INSERT_SUCCESS',
-      } as UpsertResult;
-      await afterUpsertDocument(newUpsertRequest, upsertResult, client);
+  afterAll(async () => {
+    await teardownOpenSearch();
+  });
+
+  describe('when insert was successful', () => {
+    beforeEach(async () => {
+      await afterUpsertDocument(
+        newUpsertRequest,
+        {
+          response: 'INSERT_SUCCESS',
+        } as UpsertResult,
+        client,
+      );
+    });
+
+    afterEach(async () => {
+      await afterDeleteDocumentById(
+        { id, resourceInfo } as DeleteRequest,
+        { response: 'DELETE_SUCCESS' } as DeleteResult,
+        client,
+      );
     });
 
     it('should be created', async () => {
       const response = await queryDocuments(setupQueryRequest({}, {}), client);
 
-      expect(response).toMatchInlineSnapshot(`
-        {
-          "documents": [
-            {
-              "id": "${id}",
-            },
-          ],
-          "response": "QUERY_SUCCESS",
-          "totalCount": 1,
-        }
-      `);
+      expect(response.documents).toEqual(expect.arrayContaining([expect.objectContaining({ id })]));
     });
   });
 
-  afterAll(async () => {
-    await teardownOpenSearch();
+  describe('when update was successful', () => {
+    beforeEach(async () => {
+      newUpsertRequest.traceId = 'tracer2';
+      await afterUpsertDocument(
+        newUpsertRequest,
+        {
+          response: 'UPDATE_SUCCESS',
+        } as UpsertResult,
+        client,
+      );
+    });
+
+    afterEach(async () => {
+      await afterDeleteDocumentById(
+        { id, resourceInfo } as DeleteRequest,
+        { response: 'DELETE_SUCCESS' } as DeleteResult,
+        client,
+      );
+    });
+
+    it('should be updated', async () => {
+      const response = await queryDocuments(setupQueryRequest({}, {}), client);
+
+      expect(response.documents).toEqual(expect.arrayContaining([expect.objectContaining({ id })]));
+    });
+  });
+
+  describe('when response was not successful', () => {
+    it.each([
+      'INSERT_FAILURE_REFERENCE',
+      'INSERT_FAILURE_CONFLICT',
+      'UPDATE_FAILURE_REFERENCE',
+      'UPSERT_FAILURE_AUTHORIZATION',
+      'UNKNOWN_FAILURE',
+    ])('should not insert when result is %s', async (response) => {
+      await afterUpsertDocument(
+        newUpsertRequest,
+        {
+          response,
+        } as UpsertResult,
+        client,
+      );
+
+      const result = await queryDocuments(setupQueryRequest({}, {}), client);
+
+      expect(result.documents).toHaveLength(0);
+    });
+  });
+
+  describe('when updating by id is successful', () => {
+    beforeEach(async () => {
+      await afterUpdateDocumentById(
+        newUpsertRequest,
+        {
+          response: 'UPDATE_SUCCESS',
+        } as UpdateResult,
+        client,
+      );
+    });
+
+    afterEach(async () => {
+      await afterDeleteDocumentById(
+        { id, resourceInfo } as DeleteRequest,
+        { response: 'DELETE_SUCCESS' } as DeleteResult,
+        client,
+      );
+    });
+
+    it('should be updated', async () => {
+      const response = await queryDocuments(setupQueryRequest({}, {}), client);
+
+      expect(response.documents).toEqual(expect.arrayContaining([expect.objectContaining({ id })]));
+    });
+  });
+
+  describe('when updating should not be saved', () => {
+    it.each(['UPDATE_FAILURE_REFERENCE', 'UPDATE_FAILURE_NOT_EXISTS', 'UPDATE_FAILURE_AUTHORIZATION', 'UNKNOWN_FAILURE'])(
+      'should not update when result is %s',
+      async (response) => {
+        await afterUpdateDocumentById(
+          newUpsertRequest,
+          {
+            response,
+          } as UpdateResult,
+          client,
+        );
+
+        const result = await queryDocuments(setupQueryRequest({}, {}), client);
+
+        expect(result.documents).toHaveLength(0);
+      },
+    );
   });
 });
