fc42dae7628333662e748fe384339d94a1ef743f
Stephen A. Fuqua <stephen@safnet.com>
2023-02-09 08:14:17 -0600

    License headers

--- Meadowlark-js/backends/meadowlark-mongodb-backend/docker/scripts/mongo-key-file-setup.sh
+++ Meadowlark-js/backends/meadowlark-mongodb-backend/docker/scripts/mongo-key-file-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 openssl rand -base64 700 > /auth/file.key
 chmod 400 /auth/file.key
 chown 999:999 /auth/file.key
