fb26ff7e193f6e80b3e828c923af16701df0b548
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2023-02-20 14:00:35 -0600

    Update ValidateResourceIdMiddleware.ts

--- Meadowlark-js/packages/meadowlark-core/src/middleware/ValidateResourceIdMiddleware.ts
+++ Meadowlark-js/packages/meadowlark-core/src/middleware/ValidateResourceIdMiddleware.ts
@@ -3,8 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-import { writeDebugStatusToLog, writeRequestToLog } from '../Logger';
-import { isDocumentIdValidForResource } from '../validation/DocumentIdValidator';
+import { writeRequestToLog } from '../Logger';
 import { MiddlewareModel } from './MiddlewareModel';
 
 const moduleName = 'core.middleware.ValidateResourceIdMiddleware';
