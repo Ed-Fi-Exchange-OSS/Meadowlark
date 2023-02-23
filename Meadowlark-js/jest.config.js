e4b30ac089ca2032020d5a7374695a89ed06f725
Brad Banister <brad@banisterconsult.com>
2023-02-22 20:36:44 -0600

    [RND-442] fix jest and e2e issues

--- Meadowlark-js/jest.config.js
+++ Meadowlark-js/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/tests/config/unit/jest.config.js',
     '<rootDir>/tests/config/integration/jest.config.js',
     '<rootDir>/tests/config/e2e/jest.config.js'
-  ]
+  ],
+  workerIdleMemoryLimit: '300MB',
 };
