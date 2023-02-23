a8f9bf63adc2f7fb897b0eefd2db8825a5eff21b
Stephen A. Fuqua <stephen@safnet.com>
2023-02-23 10:28:35 -0600

    Add file header

--- eng/setup-mongo.sh
+++ eng/setup-mongo.sh
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 chmod -R +x ./scripts
 docker run -d --name mongo-temp -v mongo-auth:/auth mongo:4.0.28
 docker exec mongo-temp mkdir /scripts
