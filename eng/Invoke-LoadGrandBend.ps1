d94397a44716c327656ee8f4275511d01f77ac31
Stephen A. Fuqua <stephen@safnet.com>
2023-02-23 10:54:05 -0600

    Rename file

--- eng/Invoke-LoadGrandBend.ps1
+++ eng/Invoke-LoadGrandBend.ps1
@@ -3,6 +3,8 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+# Runs the complete bulk upload of the Grand Bend dataset, aka "populated template"
+
 #Requires -Version 7
 $ErrorActionPreference = "Stop"
 
