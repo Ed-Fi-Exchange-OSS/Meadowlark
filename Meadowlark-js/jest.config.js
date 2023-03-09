316a92d64a667b5338b5daaaf984041e0cf9aaab
AndonyNS <andony91@gmail.com>
2023-03-09 09:22:11 -0600

    [RND-490] Remove duplications from jest config

--- Meadowlark-js/jest.config.js
+++ Meadowlark-js/jest.config.js
@@ -4,20 +4,6 @@ module.exports = {
   projects: [
     '<rootDir>/tests/config/unit/jest.config.js',
     '<rootDir>/tests/config/integration/jest.config.js',
-    '<rootDir>/tests/config/e2e/jest.config.js'
+    '<rootDir>/tests/config/e2e/jest.config.js',
   ],
-  coverageThreshold: {
-    global: {
-      branches: 52,
-      functions: 58,
-      lines: 60,
-      statements: 60,
-    },
-  },
-  reporters: [
-    'default',
-    'github-actions'
-  ],
-  collectCoverageFrom: ['packages/**/src/**/*.ts', 'backends/**/src/**/*.ts', 'services/**/src/**/*.ts'],
-  workerIdleMemoryLimit: '200MB',
 };
--- Meadowlark-js/tests/config/integration/jest.config.js
+++ Meadowlark-js/tests/config/integration/jest.config.js
@@ -1,13 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const defaultConfig = require('./../jest.config.js');
-const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
   displayName: 'Integration Tests',
   ...defaultConfig,
   preset: '@shelf/jest-mongodb',
   testMatch: ['<rootDir>/**/integration/**/*.(spec|test).[jt]s?(x)'],
-  watchPathIgnorePatterns: ['globalConfig'], // jest-mongodb setup
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/src/**/*.ts',
+    '<rootDir>/backends/**/src/**/*.ts',
+    '<rootDir>/services/**/src/**/*.ts',
+  ],
   coverageThreshold: {
     global: {
       branches: 52,
@@ -17,4 +20,4 @@ module.exports = {
     },
   },
   workerIdleMemoryLimit: '200MB',
-}
+};
