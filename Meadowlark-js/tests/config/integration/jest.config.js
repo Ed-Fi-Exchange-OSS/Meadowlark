95f64afa1d596c9f15794d1159624dc68e542f65
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2023-03-14 16:55:56 -0600

    Update jest.config.js

--- Meadowlark-js/tests/config/integration/jest.config.js
+++ Meadowlark-js/tests/config/integration/jest.config.js
@@ -4,6 +4,9 @@ const defaultConfig = require('./../jest.config.js');
 module.exports = {
   displayName: 'Integration Tests',
   ...defaultConfig,
+  projects: [
+    '<rootDir>/backends/meadowlark-opensearch-backend/test/jest.config.js',
+  ],
   preset: '@shelf/jest-mongodb',
   testMatch: ['<rootDir>/**/integration/**/*.(spec|test).[jt]s?(x)'],
   watchPathIgnorePatterns: ['globalConfig'], // jest-mongodb setup
