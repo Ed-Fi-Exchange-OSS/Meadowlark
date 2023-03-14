1005d99065fd2be6e287a35ac7978288c98419fb
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2023-03-13 18:56:09 -0600

    Update OpenSearchSetupEnvironment.ts
    Remove console.info

--- Meadowlark-js/backends/meadowlark-opensearch-backend/test/setup/OpenSearchSetupEnvironment.ts
+++ Meadowlark-js/backends/meadowlark-opensearch-backend/test/setup/OpenSearchSetupEnvironment.ts
@@ -5,22 +5,18 @@
 import path from 'path';
 import { DockerComposeEnvironment, StartedDockerComposeEnvironment } from 'testcontainers';
 import dotenv from 'dotenv';
+import { Logger } from 'packages/meadowlark-utilities';
 
 const containerName = 'opensearch-integration-test';
 const envFilePath = path.join(__dirname, '.env');
 let environment: StartedDockerComposeEnvironment;
 dotenv.config({ path: envFilePath });
 
-export async function stop() {
-  console.info('-- Tearing down OpenSearch environment --');
-  await environment.getContainer(containerName).stop();
-}
-
 export async function start() {
   const port = 8200;
   const composeFile = 'docker-compose.yml';
   const composeFilePath = path.resolve(__dirname, './');
-  console.info('-- Setup OpenSearch environment --');
+  Logger.info('-- Setup OpenSearch environment --', null);
   environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
     .withNoRecreate()
     .withStartupTimeout(120_000)
@@ -31,4 +27,9 @@ export async function start() {
   process.env.OPENSEARCH_USERNAME = 'admin';
   process.env.OPENSEARCH_PASSWORD = 'admin';
   process.env.OPENSEARCH_REQUEST_TIMEOUT = '10000';
+}
+
+export async function stop() {
+  Logger.info('-- Tearing down OpenSearch environment --', null);
+  await environment.getContainer(containerName).stop();
 }
