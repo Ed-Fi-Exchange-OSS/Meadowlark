6a92d44088ff654d6d87e971ff9bc389699b2dc5
Stephen A. Fuqua <stephen@safnet.com>
2023-03-17 17:29:12 -0500

    Attempting to calculate single depth

--- Meadowlark-js/version.js
+++ Meadowlark-js/version.js
@@ -3,18 +3,51 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-// This code could easily be expressed in a shell script, but using Node makes it platform independent.
+/*
+ * Calculates the next version number based on prior tag and commit depth.
+ *
+ * `git describe` returns the last tag plus the commit depth and the short hash
+ * for the most recent commit. Example: last tag was v0.3.0, there has been one
+ * more commit, and the most recent has hash `g1636c72`; then the output will be
+ * `v0.3.1-1-g1636c72`. But if the last tag was `v0.3.1-10-gbad82c2`, then the
+ * next output from `git describe` will be `v0.3.1-10-gbad82c2-1-g1636c72`. And
+ * this will keep growing with every pre-release. What we really want is
+ * `v0.3.1-pre11` (11 = 10 commits from the base, then another 1 commit).
+ *
+ * There is one command line flag: --update. When set, it will inject the
+ * calculated version into all of the package.json files. Otherwise, just writes
+ * the version to the console.
+ */
 
 const { execSync } = require('child_process');
 
-const callShellCommand = (cmd) => {
-  // eslint-disable-next-line no-console
-  console.info(`executing: ${cmd}`);
-  return execSync(cmd).toString().replace('\n', '');
-};
+const updatePackageJson = process.argv.includes('--update');
 
-// `git describe` returns the last tag plus the commit depth and the short hash for the most recent commit
-// Example: last tag was v1.0.0, there have been four more commits, and the most recent has hash abcdef. Then
-// the output will be v1.0.0-4-abcdef
-const version = callShellCommand('git describe --first-parent --tags');
-callShellCommand(`npm version ${version} --allow-same-version --workspaces true`);
+const callShellCommand = (cmd) => execSync(cmd).toString().replace('\n', '');
+
+const gitDescribe = callShellCommand('git describe --first-parent --tags');
+
+// Retrieve the base version number
+const versionExpr = /^v([^-]+)/;
+const versionMatch = gitDescribe.match(versionExpr);
+if (!versionMatch) {
+  // eslint-disable-next-line no-console
+  console.error(`Unable to extract a base version number from ${gitDescribe}`);
+  process.exit(1);
+}
+
+// Parse out the depth markers, and add them together
+const depthExpr = /-(\d+)-.{8}/gm;
+let depth = 0;
+[...gitDescribe.matchAll(depthExpr)].forEach((match) => {
+  depth += Number.parseInt(match[1], 10);
+});
+
+// Build the new version string
+const version = `${versionMatch[0]}-pre${depth > 0 ? depth.toString() : ''}`;
+// eslint-disable-next-line no-console
+console.info(version);
+
+if (updatePackageJson) {
+  callShellCommand(`npm version ${version} --allow-same-version --workspaces true`);
+}
