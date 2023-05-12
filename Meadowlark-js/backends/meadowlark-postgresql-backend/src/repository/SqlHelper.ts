cb71e942968d848d6188a8d7149d22f7b0ebe545
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2023-05-12 13:13:52 -0600

    Fix linting error

--- Meadowlark-js/backends/meadowlark-postgresql-backend/src/repository/SqlHelper.ts
+++ Meadowlark-js/backends/meadowlark-postgresql-backend/src/repository/SqlHelper.ts
@@ -2,7 +2,7 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
-import { DocumentUuid, MeadowlarkId } from 'packages/meadowlark-core';
+import { DocumentUuid, MeadowlarkId } from '@edfi/meadowlark-core';
 import format from 'pg-format';
 
 // SQL for Selects
