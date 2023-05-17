73acbd68ab608fd680fc6c7f415780a3aa82f299
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2023-05-17 12:59:16 -0600

    Update test to add location

--- Meadowlark-js/packages/meadowlark-core/src/middleware/ParsePathMiddleware.ts
+++ Meadowlark-js/packages/meadowlark-core/src/middleware/ParsePathMiddleware.ts
@@ -4,7 +4,7 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 import { writeDebugStatusToLog, writeRequestToLog } from '../Logger';
-import { isMeadowlarkIdWellFormed, isDocumentUuidWellFormed } from '../validation/DocumentIdValidator';
+import { isDocumentUuidWellFormed } from '../validation/DocumentIdValidator';
 import type { PathComponents } from '../model/PathComponents';
 import type { DocumentUuid } from '../model/BrandedTypes';
 import type { MiddlewareModel } from './MiddlewareModel';
