408c2440439bc59791b6ab0927178164347e71da
Andony Nuñez <andony91@gmail.com>
2023-05-22 20:22:58 -0600

    Adjusting script to exit dev containers to handle wsl1

--- eng/exit-dev-containers.sh
+++ eng/exit-dev-containers.sh
@@ -1,13 +1,22 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
 # SPDX-License-Identifier: Apache-2.0
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-#!/bin/bash
+docker=docker
+if [[ $(grep Microsoft /proc/version) ]]; then
+# Running under WSL1, need to reference docker.exe instead of docker. Simple
+# alias doesn't work, sadly. This variable helps in the context of this
+# script, but need to use "wsl1" commands with npm.
+docker=docker.exe
+fi
 
-docker ps -q --filter "name=mongo-test" | grep -q . && docker stop mongo-test && docker rm -f mongo-test
-docker ps -q --filter "name=postgres-test" | grep -q . && docker stop postgres-test && docker rm -f postgres-test
-docker ps -q --filter "name=opensearch-test" | grep -q . && docker stop opensearch-test && docker rm -f opensearch-test
-docker ps -q --filter "name=meadowlark-api-test" | grep -q . && docker stop meadowlark-api-test && docker rm -f meadowlark-api-test
+for container in "mongo-test" "postgres-test" "opensearch-test" "meadowlark-api-test"
+do
+  docker ps -q --filter "name=$container" | grep -q . && docker stop $container && docker rm -f $container
+done
 
 docker network prune -f
