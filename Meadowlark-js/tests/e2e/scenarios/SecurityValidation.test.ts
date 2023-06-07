2b2a628e499814d6da7cb9eb07745df75ed87a89
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2023-06-07 14:39:44 -0600

    [RND-556] Faulty ownership security on POST requests
    Add test to validate ownership on insert
    Update ownership validation, to validate first by documentUuid, if that id is null, it tries to find the document by meadowlarkId.

--- Meadowlark-js/tests/e2e/scenarios/SecurityValidation.test.ts
+++ Meadowlark-js/tests/e2e/scenarios/SecurityValidation.test.ts
@@ -228,6 +228,19 @@ describe('given the existence of two vendors and one host', () => {
       });
     });
 
+    describe('when a different vendor tries to insert an existing resource', () => {
+      it('should return error', async () => {
+        await rootURLRequest()
+          .post(`/v3.3b/ed-fi/${resourceEndpoint}`)
+          .auth(vendor2DataAccessToken, { type: 'bearer' })
+          .send(resourceBodyPutUpdated)
+          .expect(403)
+          .then((response) => {
+            expect(response.body).toBe('');
+          });
+      });
+    });
+
     describe('when a different vendor updates the resource', () => {
       it('should return error', async () => {
         await rootURLRequest()
