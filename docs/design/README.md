87a95de354786407e6c5eb5c175b3a68de246159
Andony Núñez <andony91@gmail.com>
2023-06-08 14:05:01 -0600

    [RND-533] Technical design for get all performance benchmarking (#251)
    * [RND-533] Initial Instructions
    
    * Updating ticket link
    
    * [RND-533] Adding instructions to run the mongo profiler
    
    * Adding missing column in results example
    
    * Adding profiler section collapsed
    
    * Change ticket name
    
    * [RND-533] Adding script to run performance multiple tests, updating instructions
    
    * Add spacing
    
    * Changing page title to handle read and write separately

--- docs/design/README.md
+++ docs/design/README.md
@@ -3,3 +3,4 @@
 ## Contents
 
 * [Support for Offline Cascading Updates](offline-cascading-updates/)
+* [Read Performance Benchmarking](read-performance-benchmarking/)
---
+++ eng/performance/GetAll-Performance.ps1
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+<#
+.DESCRIPTION
+    Run performance tests multiple times
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $PagingTestsPath,
+
+    [int]
+    $NumTrials = 5
+
+)
+
+$times = @()
+
+$originalLocation = Get-Location
+
+Set-Location -Path $PagingTestsPath
+for ($i = 0; $i -lt $NumTrials; $i++) {
+    $timing = Measure-Command { poetry run python edfi_paging_test }
+    $times += $timing.TotalMilliseconds
+}
+
+$sum = 0.0
+$times | ForEach-Object { $sum += $_ }
+$mean= $sum / $NumTrials
+
+$sumSquareError = 0.0
+$times | ForEach-Object { $sumSquareError += [Math]::Pow($_ - $mean, 2) }
+$standardDeviation = [Math]::Sqrt($sumSquareError / $NumTrials)
+
+Write-Output @"
+Mean: $mean
+Standard Deviation: $standardDeviation
+"@
+
+Set-Location -Path $originalLocation
