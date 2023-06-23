2b31169a82a2ed42164f01a4f69664c19ef3a0d0
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2023-06-22 20:39:26 -0600

    Integrates Elastic Search tests into CI process.

--- Meadowlark-js/tests/e2e/setup/EnvironmentConfig.ts
+++ Meadowlark-js/tests/e2e/setup/EnvironmentConfig.ts
@@ -7,16 +7,26 @@ import { Network, StartedNetwork } from 'testcontainers';
 import * as ApiContainer from './containers/ApiContainer';
 import * as MongoContainer from './containers/MongoContainer';
 import * as OpenSearchContainer from './containers/OpenSearchContainer';
+import * as ElasticSearchContainer from './containers/ElasticSearchContainer';
 import * as PostgreSqlContainer from './containers/PostgresqlContainer';
 import { endLog } from './LogConfig';
 
 export async function stop() {
   endLog();
   console.info('-- Tearing down environment --');
-  await Promise.all([MongoContainer.stop(), ApiContainer.stop(), OpenSearchContainer.stop()]);
+  await Promise.all([MongoContainer.stop(), ApiContainer.stop()]);
+
   if (process.env.DOCUMENT_STORE_PLUGIN === '@edfi/meadowlark-postgresql-backend') {
     console.info('-- Tearing down postgres --');
     await PostgreSqlContainer.stop();
+  }
+
+  if (process.env.QUERY_HANDLER_PLUGIN === '@edfi/meadowlark-elasticsearch-backend') {
+    console.info('-- Setting up elasticsearch --');
+    await ElasticSearchContainer.stop();
+  } else {
+    console.info('-- Setting up opensearch --');
+    await OpenSearchContainer.stop();
   }
 }
 
@@ -35,11 +45,19 @@ export async function configure(initialize = true) {
   } else {
     console.info('-- Setting up containers --');
     // Starting Mongo container since it's required for Authentication
-    await Promise.all([MongoContainer.setup(network), ApiContainer.setup(network), OpenSearchContainer.setup(network)]);
+    await Promise.all([MongoContainer.setup(network), ApiContainer.setup(network)]);
 
     if (process.env.DOCUMENT_STORE_PLUGIN === '@edfi/meadowlark-postgresql-backend') {
       console.info('-- Setting up postgres --');
       await PostgreSqlContainer.setup(network);
+    }
+
+    if (process.env.QUERY_HANDLER_PLUGIN === '@edfi/meadowlark-elasticsearch-backend') {
+      console.info('-- Setting up elasticsearch --');
+      await ElasticSearchContainer.setup(network);
+    } else {
+      console.info('-- Setting up opensearch --');
+      await OpenSearchContainer.setup(network);
     }
   }
 
---
+++ Meadowlark-js/tests/e2e/setup/containers/ElasticSearchContainer.ts
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+import { GenericContainer, StartedNetwork, StartedTestContainer } from 'testcontainers';
+import { setElasticSearchLog } from '../LogConfig';
+
+let startedContainer: StartedTestContainer;
+
+export async function setup(network: StartedNetwork) {
+  try {
+    const elasticSearchPort = parseInt(process.env.ELASTICSEARCH_PORT ?? '8200', 10);
+    const container = new GenericContainer(
+      'docker.elastic.co/elasticsearch/elasticsearch:8.8.0@sha256:9aaa38551b4d9e655c54d9dc6a1dad24ee568c41952dc8cf1d4808513cfb5f65',
+    )
+      .withName('elasticsearch-node-test')
+      .withNetwork(network)
+      .withReuse()
+      .withExposedPorts({
+        container: 9200,
+        host: elasticSearchPort,
+      })
+      .withEnvironment({
+        'node.name': 'es01',
+        'cluster.name': 'elasticsearch-node1',
+        'discovery.type': 'single-node',
+        'xpack.security.enabled': 'false',
+      })
+      .withStartupTimeout(120_000);
+
+    startedContainer = await container.start();
+  } catch (error) {
+    throw new Error(`\nUnexpected error setting up elastic search container:\n${error}`);
+  }
+
+  await setElasticSearchLog(startedContainer);
+}
+
+export async function stop(): Promise<void> {
+  await startedContainer.stop();
+}
