0ef2da7564f990527028988f5de76f19582b1997
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2023-06-26 14:57:42 -0600

    Fixing more e2e tests.

--- Meadowlark-js/backends/meadowlark-elasticsearch-backend/src/repository/QueryElasticsearch.ts
+++ Meadowlark-js/backends/meadowlark-elasticsearch-backend/src/repository/QueryElasticsearch.ts
@@ -79,9 +79,17 @@ export async function queryDocuments(request: QueryRequest, client: Client): Pro
 
     Logger.debug(`${moduleName}.queryDocuments queryDocuments executing query: ${query}`, traceId);
 
+    const index = indexFromResourceInfo(resourceInfo);
+    const indexexists = await client.indices.exists({ index });
+
+    if (indexexists === false) {
+      Logger.info(`${moduleName}.queryDocuments: Index not found`, traceId);
+      return { response: 'QUERY_SUCCESS', documents, totalCount: 0 };
+    }
+
     const body = await performDslQuery(
       client,
-      indexFromResourceInfo(resourceInfo),
+      index,
       query,
       paginationParameters.limit as number,
       paginationParameters.offset as number,
