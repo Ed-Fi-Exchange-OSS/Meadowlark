c7d5bb4435453275f072202272c25b524fca017e
Stephen A. Fuqua <stephen@safnet.com>
2023-08-02 13:56:14 -0500

    Revamp the custom Mongo image

--- docker/mongodb/initialize-cluster.sh
+++ docker/mongodb/initialize-cluster.sh
@@ -6,79 +6,79 @@ set -e -o pipefail
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-if [ ! $HOSTNAME == "mongo1" ]; then
+# Initialize 3-node replica setup
+echo ">>>> Start MongoDB replica setup <<<<"
+
+# Start Mongo in regular mode, in the background. Capture PID to enable
+# killing it later.
+echo "Start Mongo"
+/usr/bin/mongod --bind_ip_all --replSet rs0 --keyFile /auth/file.key &
+
+# Sleep again to let mongod startup
+sleep 10
+
+if [ ! $? == 0 ]
+then
+  >&2 echo "Unable to start MongoDB service, exiting script"
   exit
 fi
 
-# Create the security key file if needed
-if [ ! -f /auth/file.key ]; then
-  openssl rand -base64 700 > /auth/file.key
-  chmod 400 /auth/file.key
-  chown 999:999 /auth/file.key
-fi
-
-# Initialize 3-node replica setup if needed
-if [ ! -f /auth/replicaSetup ]; then
-
-  if [ ! $? == 0 ]; then
-    >&2 echo "Unable to start MongoDB service, exiting script"
-    exit
-  fi
-
-  echo "Initializing MongoDB Replication"
-  command="var cfg = {
-  \"_id\": \"rs0\",
-  \"protocolVersion\": 1,
-  \"version\": 1,
-  \"members\": [
-      {
-          \"_id\": 0,
-          \"host\": \"mongo1:27017\",
-          \"priority\": 2
-      },
-      {
-          \"_id\": 1,
-          \"host\": \"mongo2:27018\",
-          \"priority\": 0
-      },
-      {
-          \"_id\": 2,
-          \"host\": \"mongo3:27019\",
-          \"priority\": 0
-      }
-  ],settings: {chainingAllowed: true}
+echo "Initializing MongoDB Replication"
+command="var cfg = {
+\"_id\": \"rs0\",
+\"protocolVersion\": 1,
+\"version\": 1,
+\"members\": [
+    {
+        \"_id\": 0,
+        \"host\": \"mongo1:27017\",
+        \"priority\": 2
+    },
+    {
+        \"_id\": 1,
+        \"host\": \"mongo2:27017\",
+        \"priority\": 0
+    },
+    {
+        \"_id\": 2,
+        \"host\": \"mongo3:27017\",
+        \"priority\": 0
+    }
+],settings: {chainingAllowed: true}
 };
 rs.initiate(cfg, { force: true });
 rs.status();
-  "
-  mongo --eval "$command"
+"
+mongo --eval "$command"
 
-  if [ ! $? == 0 ]; then
-    >&2 echo "Unable to initialize MongoDB replication, exiting script"
-    kill -9 $pid
-    exit
-  fi
+if [ ! $? == 0 ]
+then
+  >&2 echo "Unable to initialize MongoDB replication, exiting script"
+  mongod --shutdown
+  exit
+fi
 
-  echo "Sleeping for 20 seconds to let replication take hold.."
-  sleep 20
+echo "Sleeping to let replication take hold"
+sleep 15
 
-  command="use admin;
-admin = db.getSiblingDB(\"admin\");
-admin.createUser(
-{
-  user: \"$ADMIN_USERNAME\",
-  pwd: \"$ADMIN_PASSWORD\",
-  roles: [ { role: \"root\", db: \"admin\" } ]
+command="admin = db.getSiblingDB(\"admin\");
+admin.createUser({
+user: \"$ADMIN_USERNAME\",
+pwd: \"$ADMIN_PASSWORD\",
+roles: [ { role: \"root\", db: \"admin\" } ]
 });
 db.getSiblingDB(\"admin\").auth(\"$ADMIN_USERNAME\", \"$ADMIN_PASSWORD\");
 "
-  mongo --eval "$command"
+mongo --eval "$command"
 
-  if [ ! $? == 0 ]; then
-    >&2 echo "Unable to setup authentication, exiting script"
-    exit
-  fi
-
-  # Prevent this block from running again
-  touch /auth/replicaSetup
+if [ ! $? == 0 ]
+then
+  >&2 echo "Unable to setup authentication, exiting script"
+  mongod --shutdown
+  exit
 fi
+
+mongod --shutdown
+
+# Prevent this block from running again
+touch /auth/replicaSetup
