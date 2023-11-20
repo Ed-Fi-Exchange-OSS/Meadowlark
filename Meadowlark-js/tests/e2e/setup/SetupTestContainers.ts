f3d8efdade4079b865398c317fb704de9210e0fc
Andony Nuñez <andony91@gmail.com>
2023-11-20 12:02:11 -0600

    E2E - Add option to run only postgresql

--- Meadowlark-js/tests/e2e/setup/SetupTestContainers.ts
+++ Meadowlark-js/tests/e2e/setup/SetupTestContainers.ts
@@ -45,14 +45,21 @@ export async function configure(initialize = true) {
   } else {
     console.info('-- Setting up containers --');
 
-    if (process.env.DOCUMENT_STORE_PLUGIN === '@edfi/meadowlark-postgresql-backend') {
+    if (
+      process.env.DOCUMENT_STORE_PLUGIN === '@edfi/meadowlark-postgresql-backend' ||
+      process.env.AUTHORIZATION_STORE_PLUGIN === '@edfi/meadowlark-postgresql-backend'
+    ) {
       console.info('-- Setting up postgres --');
       await PostgreSqlContainer.setup(network);
     } else {
+      console.info('-- Setting up mongo --');
       await MongoContainer.setup(network);
     }
 
-    if (process.env.QUERY_HANDLER_PLUGIN === '@edfi/meadowlark-elasticsearch-backend') {
+    if (
+      process.env.QUERY_HANDLER_PLUGIN === '@edfi/meadowlark-elasticsearch-backend' ||
+      process.env.LISTENER1_PLUGIN === '@edfi/meadowlark-elasticsearch-backend'
+    ) {
       console.info('-- Setting up elasticsearch --');
       await ElasticSearchContainer.setup(network);
     } else {
