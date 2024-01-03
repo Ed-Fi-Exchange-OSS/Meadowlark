3dd6f8e69363c8090114439d63f3db125d2291bb
Stephen A. Fuqua <stephen@safnet.com>
2024-01-03 11:27:00 -0600

    Add missing file headers

--- Meadowlark-js/tests/e2e/scenarios/SecurityValidation.test.ts
+++ Meadowlark-js/tests/e2e/scenarios/SecurityValidation.test.ts
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 import { Credentials, createClient, getAccessToken } from '../helpers/Credentials';
 import { createResource, deleteResourceByLocation } from '../helpers/Resources';
 import { baseURLRequest, rootURLRequest } from '../helpers/Shared';
